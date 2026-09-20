9c3f2d751374b2e3294ffa0cf1cd46b64d3eb796
Adnan <118480749+aribadnan@users.noreply.github.com>
2026-09-20 16:06:40 +0600

    Create Task_1.R

---
+++ Task_1.R
@@ -0,0 +1,11 @@
+name <- "Adnan"
+age <- 21
+dept <- "Data Science"
+
+cat(paste0("As-salamu alaykum,\nI am ", name, ", ", age, " years old,\nStudent of ", dept, " at Jahangirnagar University;"))
+# In R, paste0() is used to join (concatenate) strings together without adding any separator.
+
+# Output:
+# As-salamu alaykum,
+# I am Adnan, 21 years old,
+# Student of Data Science at Jahangirnagar University;
